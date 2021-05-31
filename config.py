90bcc8916995406158af262609dec57441e61df7
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2021-05-31 22:15:45 +0500

    Updating PORT

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ import os
 class DefaultConfig:
     """ Bot Configuration """
 
-    PORT = 3978
+    PORT = 8000
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
