74cd8b2add513232ad1849d5de476925e92e08e3
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2021-05-31 23:45:18 +0500

    Update ourbot.py

--- ourbot.py
+++ ourbot.py
@@ -16,6 +16,8 @@ class ActivityBot(ActivityHandler):
             chat = Chat(self.pairs)
             print(type(turn_context.activity.text))
             await turn_context.send_activity(chat.respond(turn_context.activity.text))
+
+            
         async def on_members_added_activity(self,member_added : ChannelAccount,turn_context:TurnContext):
             for member in member_added:
                 await turn_context.send_activity(member.name)  
