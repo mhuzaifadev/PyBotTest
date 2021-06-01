703ab09c7f793802bc8dddc7f30d167b40c6622d
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2021-06-01 10:28:40 +0500

    APP_ID & APP_PWD

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings
 import asyncio
 
 from ourbot import ActivityBot
-
+from config import DefaultConfig
 app = Flask(__name__)
 loop = asyncio.get_event_loop()
 
 ebot = ActivityBot()
+CONFIG = DefaultConfig()
 
 #https://docs.microsoft.com/en-us/javascript/api/botbuilder/botframeworkadapter?view=botbuilder-ts-latest
 # Microsoft App ID and Microsoft App Password     "33787288-ec92-4419-b42c-363e012629bb","af6dd3ef-d22b-46e2-bbe5-4e1cd038f61c"
-botadaptersettings = BotFrameworkAdapterSettings("","")
+botadaptersettings = BotFrameworkAdapterSettings(CONFIG.APP_ID,CONFIG.APP_PASSWORD)
 botadapter = BotFrameworkAdapter(botadaptersettings)
 
 @app.route("/api/messages",methods=["POST"])
